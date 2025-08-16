322a79e3cf0cf85ddd600320076d384185a65fa8
Blake Brandon <50201165+blakebrandon-hub@users.noreply.github.com>
2025-08-16 09:24:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/download/oop-guide')
+def download_pdf():
+    return send_file("static/oop_guide.pdf", as_attachment=False)
 
 if __name__ == "__main__":
+
 	app.run(debug=True)
